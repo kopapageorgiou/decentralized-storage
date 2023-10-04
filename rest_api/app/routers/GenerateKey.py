05b8e94544ba679a2da5d9e757e2d71ead929b6d
kopapageorgiou <kopapageorgiou@uth.gr>
2023-10-04 13:11:25 +0000

    Fixed key generation

--- rest_api/app/routers/GenerateKey.py
+++ rest_api/app/routers/GenerateKey.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
+from utils.abe_api import ABEAPI
+abe_api = ABEAPI(f"http://{os.environ['ABE_HOST']}:12345")
 
 router = APIRouter()
 logging.basicConfig(level=logging.DEBUG)
@@ -16,10 +18,12 @@ class Device(BaseModel):
 @router.post('/generateKey')
 def generate_key(device: Device, request: Request):
     try:
-        request.app.state.gate_stops[device.gateway_id].remove(device.poi_id)
-        logging.debug("Stations: "+str(request.app.state.gate_stops[device.gateway_id]))
-        request.app.state.gate_keys[device.gateway_id] = base64.b64encode(get_random_bytes(32)).decode('utf-8')
-        logging.debug(request.app.state.gate_keys)
+        request.app.state.gate_stops[device.device_id].remove(device.poi_id)
+        #logging.debug("Stations: "+str(request.app.state.gate_stops[device.device_id]))
+        request.app.state.gate_keys[device.device_id] = base64.b64encode(get_random_bytes(32)).decode('utf-8')
+        request.app.state.encrypted_symmetric_keys[device.device_id] = abe_api.cp_encrypt(request.app.state.gate_stops[device.device_id],
+                                                                                            request.app.state.gate_keys[device.device_id])
+        #logging.debug(request.app.state.gate_keys)
         return {"info": "Key changed", "code": 1}
     except Exception as e:
         logging.debug(e)
--- rest_api/app/routers/Legs.py
+++ rest_api/app/routers/Legs.py
@@ -1,9 +1,12 @@
-import os
+import os, base64
 from fastapi import APIRouter, Request
 from pydantic import BaseModel
 import logging
+from Crypto.Random import get_random_bytes
+from utils.abe_api import ABEAPI
 
 router = APIRouter()
+abe_api = ABEAPI(f"http://{os.environ['ABE_HOST']}:12345")
 logging.basicConfig(level=logging.DEBUG)
 
 class Leg(BaseModel):
@@ -14,6 +17,9 @@ class Leg(BaseModel):
 def insert_leg(leg: Leg, request: Request):
     try:
         request.app.state.gate_stops[leg.device_id] = leg.leg
+        request.app.state.gate_keys[leg.device_id] = base64.b64encode(get_random_bytes(32)).decode('utf-8')
+        request.app.state.encrypted_symmetric_keys[leg.device_id] = abe_api.cp_encrypt(request.app.state.gate_stops[leg.device_id],
+                                                                                            request.app.state.gate_keys[leg.device_id])
         return {"info": "Leg has been imported successfully", "code": 1}
     except Exception as e:
         return {"info": e, "code": 0}
